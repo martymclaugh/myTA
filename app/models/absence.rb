ed54fb1494fdc80d163f1d4230d9eb194cfc73dd
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-25 11:06:56 -0700

    change absent to absence, add student question join table

---
+++ app/models/absence.rb
@@ -0,0 +1,2 @@
+class Absence < ApplicationRecord
+end
--- db/migrate/20160825175749_create_absences.rb
+++ db/migrate/20160825175749_create_absences.rb
@@ -1,7 +1,7 @@
 class CreateAbsences < ActiveRecord::Migration[5.0]
   def change
     create_table :absences do |t|
-      t.boolean :absent
+      t.boolean :absence
 
       t.timestamps
     end
