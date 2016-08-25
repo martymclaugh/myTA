73d89c0f41e7d88f724f9968d5898f5d31d53c47
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-25 11:21:00 -0700

    define login and logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    teacher = Teacher.find_by_email(params[:email])
+    if teacher && teacher.authenticate(params[:password])
+      session[:id] = teacher.id
+    end
+  end
+
+  def destroy
+    session[:id] = nil
+    redirect_to '/'
+  end
 
 end
