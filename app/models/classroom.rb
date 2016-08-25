67515cc35dd059461bcbf3ba3452ec380dd1413f
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-25 11:29:54 -0700

    add teacher and classroom associations

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,4 @@
 class Classroom < ApplicationRecord
+  belongs_to :teacher
+  has_many :students, through: :student_classroom_join_table
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,4 @@
 class Teacher < ApplicationRecord
   has_secure_password
+  has_many :classrooms
 end
