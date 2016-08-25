a8efaf10bc094bd966f9ad3ae43c2ae969f42459
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-25 11:19:06 -0700

    create register method in teachers controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,3 @@
 class SessionsController < ApplicationController
+
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,16 @@
 class TeachersController < ApplicationController
+  def create
+    if params[:teacher][:password] == params[:teacher][:password_confirmation]
+      teacher = Teacher.new(teacher_params)
+    end
+    if teacher.save
+      session[:id] = teacher.id
+    end
+  end
+
+  protected
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :email, :password)
+  end
 end
