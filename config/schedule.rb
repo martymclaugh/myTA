b05d4db9896bed93d0c7d0bb6e8545e9e8899524
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-26 12:49:44 -0700

    update time for testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 1.day, :at => '12:45 pm' do
+every 1.day, :at => '12:55 pm' do
   Student.all.each do |student|
     a = Absence.create(absence: true)
     AbsenceStudent.create(student_id: student.id, absence_id: a.id)
