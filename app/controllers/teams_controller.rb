a2a70eb0fb0a7bdaa2a66d9cd4a2ea62fbdede61
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-26 10:21:05 -0700

    create basic logic to create teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,16 @@
 class TeamsController < ApplicationController
   def create
-    p params
+    @number = params[:teams].to_i
+    @students = []
+    Classroom.find(params[:classroom]).students.each do |student|
+      @students << student.name
+    end
+    @teams = @students.shuffle.in_groups(@number)
+    # @teams.each do |team|
+    #   t = Team.create(name: Faker::Team.creature.capitalize)
+    #   team.each do |student|
+    #     StudentTeam.create(student_id: student.id, team_id: t.id)
+    #   end
+    # end
   end
 end
--- db/migrate/20160826162529_create_teams.rb
+++ db/migrate/20160826162529_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
-
+      t.string :name
       t.timestamps
     end
   end
