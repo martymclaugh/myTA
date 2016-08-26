b4e22a162e52c62042e9af2864204c70023470dc
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-26 09:54:03 -0700

    add form to create teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826162529) do
+ActiveRecord::Schema.define(version: 20160826164128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20160826162529) do
   create_table "student_questions", force: :cascade do |t|
     t.integer "student_id"
     t.integer "question_id"
+  end
+
+  create_table "student_teams", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
