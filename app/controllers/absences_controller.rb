de09262f90ae609d2babc41e6b8ef5cf6e34db21
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-26 12:11:05 -0700

    create update route in absences controller

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,2 +1,7 @@
 class AbsencesController < ApplicationController
+  def index
+    @student = Student.find(params[:student_id])
+    @student.absences.last.update(absence: false)
+    redirect_to "/teachers/#{params[:teacher_id]}/classrooms/#{params[:classroom_id]}"
+  end
 end
