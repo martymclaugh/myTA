927bd82fd1d854492a250666489d2e45b83d57d8
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-26 11:42:46 -0700

    create an absence object for student whenever student is created

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,8 @@ class StudentsController < ApplicationController
     @student = Student.new(name: params[:name])
     if @student.save
       ClassroomStudent.create(student_id: @student.id, classroom_id: params[:classroom_id])
+      @absence = Absence.create(absence: true)
+      AbsenceStudent.create(student_id: @student.id, absence_id: @absence.id)
       redirect_to "/teachers/#{params[:teacher_id]}/classrooms/#{params[:classroom_id]}"
     end
   end
