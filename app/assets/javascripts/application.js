b6bb3db6ba4810c58bc360321b5d407c528a5b63
Marty McLaughlin <martymclaugh@gmail.com>
2016-09-01 15:23:50 -0700

    add turbolinks jquery gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery.turbolinks
 $(document).ready(function() {
     toggleTrueRandom();
     toggleCyclicRandom();
