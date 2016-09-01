d88d1dcfc2fcc0faa8c02e3ad72ed0ec06b9c8be
Marty McLaughlin <martymclaugh@gmail.com>
2016-09-01 12:21:13 -0700

    change absence to false upon creating student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
     @student = Student.new(name: params[:name])
     if @student.save
       ClassroomStudent.create(student_id: @student.id, classroom_id: params[:classroom_id])
-      @absence = Absence.create(absence: true)
+      @absence = Absence.create(absence: false)
       AbsenceStudent.create(student_id: @student.id, absence_id: @absence.id)
       redirect_to "/teachers/#{params[:teacher_id]}/classrooms/#{params[:classroom_id]}"
     end
